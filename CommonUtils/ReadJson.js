78bfed491568f52502c6af28ea6b26b646b36096
sarthakmehta10 <sarthak_mehta@epam.com>
2016-09-01 17:36:53 +0530

    Update ReadJson.js

--- CommonUtils/ReadJson.js
+++ CommonUtils/ReadJson.js
@@ -8,15 +8,22 @@ function readTestdataJson(json, scenarioName, testcaseName, columnName){
 }
 function readSqldataJson(id, columnName){
     var conn = readsql.createConnection();
-    var query = readsql.executeQuery(conn, "select * from account");
-    
+    var data;
+    conn.query("select * from account", function(err, rows, fields){
+        if (err) throw err; 
+        console.log("Executing query");
+        data = rows;
+        console.log(JSON.stringify(rows));
+//        return rows;
+    });
+    //var query = readsql.executeQuery(conn, "select * from account");
     //query.next();
     //console.log(query.next().value);
     var jsonpath = require('D:/TESTING/Protractor/npm-3.10.6/bin/node_modules/jsonpath-plus');
 	var result;
     console.log( '$..[?(@.id == "' + id + '")].' + columnName + '');
-    console.log(query);
-	result = jsonpath({json: query, path: '$..[?(@.id == "' + id + '")].' + columnName + ''});
+    console.log(data);
+	result = jsonpath({json: data, path: '$..[?(@.id == "' + id + '")].' + columnName + ''});
 	console.log(columnName + ": " + result);
 	return result;
 }
